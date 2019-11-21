92aced67bb2bea03a01d8effc23603b3ce854776
adolf pizzamannen <joel.magnusso@elev.ga.ntig.se>
2019-11-21 08:40:11 +0100

    la till kalender

--- Kalender/Project1/MyForm.h
+++ Kalender/Project1/MyForm.h
@@ -36,6 +36,11 @@ namespace Project1 {
 			}
 		}
 
+	private: System::Windows::Forms::MonthCalendar^  monthCalendar1;
+	protected:
+
+	protected:
+
 	private:
 		/// <summary>
 		/// Required designer variable.
@@ -49,12 +54,34 @@ namespace Project1 {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->components = gcnew System::ComponentModel::Container();
-			this->Size = System::Drawing::Size(300,300);
-			this->Text = L"MyForm";
-			this->Padding = System::Windows::Forms::Padding(0);
+			this->monthCalendar1 = (gcnew System::Windows::Forms::MonthCalendar());
+			this->SuspendLayout();
+			// 
+			// monthCalendar1
+			// 
+			this->monthCalendar1->BackColor = System::Drawing::SystemColors::Desktop;
+			this->monthCalendar1->ForeColor = System::Drawing::SystemColors::Desktop;
+			this->monthCalendar1->Location = System::Drawing::Point(591, 18);
+			this->monthCalendar1->Name = L"monthCalendar1";
+			this->monthCalendar1->TabIndex = 1;
+			this->monthCalendar1->TitleBackColor = System::Drawing::SystemColors::ActiveCaptionText;
+			this->monthCalendar1->TrailingForeColor = System::Drawing::SystemColors::ActiveCaptionText;
+			// 
+			// MyForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(1326, 610);
+			this->Controls->Add(this->monthCalendar1);
+			this->Name = L"MyForm";
+			this->Text = L"MyForm";
+			this->ResumeLayout(false);
+
 		}
 #pragma endregion
+	private: System::Void button1_Click(System::Object^  sender, System::EventArgs^  e) {
+	}
+	private: System::Void panel1_Paint(System::Object^  sender, System::Windows::Forms::PaintEventArgs^  e) {
+	}
 	};
 }

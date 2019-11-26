8c5f5455e16d1e342a2025b57307b146c39367a0
adolf pizzamannen <joel.magnusso@elev.ga.ntig.se>
2019-11-26 11:01:50 +0100

    försök att få libcurl att funka

--- Kalender/Project1/MyForm.cpp
+++ Kalender/Project1/MyForm.cpp
@@ -1,6 +1,7 @@
 #include "MyForm.h"
 #include "accountstuff.h"
 #include <nlohmann/json.hpp>
+#include <curl/curl.h>
 using namespace System;
 using namespace System::Windows::Forms;
 [STAThreadAttribute]
@@ -9,4 +10,12 @@ void Main(array<String^>^ args) {
 	Application::SetCompatibleTextRenderingDefault(false);
 	Project1::MyForm form;
 	Application::Run(%form);
+
+	CURL *curl = curl_easy_init();
+	if (curl) {
+		CURLcode res;
+		curl_easy_setopt(curl, CURLOPT_URL, "http://example.com");
+		res = curl_easy_perform(curl);
+		curl_easy_cleanup(curl);
+	}
 }

7e169edc7bc210c424ceae7a873bef10f7902ed4
adolf pizzamannen <joel.magnusso@elev.ga.ntig.se>
2019-11-28 15:27:11 +0100

    curl flyttad

--- Kalender/Project1/MyForm.cpp
+++ Kalender/Project1/MyForm.cpp
@@ -1,7 +1,7 @@
 #include "MyForm.h"
 #include "accountstuff.h"
 #include <nlohmann/json.hpp>
-#include <curl/curl.h>
+
 using namespace System;
 using namespace System::Windows::Forms;
 [STAThreadAttribute]
@@ -11,8 +11,5 @@ void Main(array<String^>^ args) {
 	Project1::MyForm form;
 	//Application::Run(%form);
 
-	CURL *curl;
 
-	curl = curl_easy_init();
-	curl_easy_cleanup(curl);
 }

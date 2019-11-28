14eeb92a2c0aedc54b43b6c0c8ecb19f2728b3aa
adolf pizzamannen <joel.magnusso@elev.ga.ntig.se>
2019-11-28 12:53:59 +0100

    bytte tillbaka till curl för att det funkar i annat projekt

--- Kalender/Project1/MyForm.cpp
+++ Kalender/Project1/MyForm.cpp
@@ -1,7 +1,7 @@
 #include "MyForm.h"
 #include "accountstuff.h"
 #include <nlohmann/json.hpp>
-
+#include <curl/curl.h>
 using namespace System;
 using namespace System::Windows::Forms;
 [STAThreadAttribute]
@@ -9,7 +9,10 @@ void Main(array<String^>^ args) {
 	Application::EnableVisualStyles();
 	Application::SetCompatibleTextRenderingDefault(false);
 	Project1::MyForm form;
-	Application::Run(%form);
+	//Application::Run(%form);
 
+	CURL *curl;
 
+	curl = curl_easy_init();
+	curl_easy_cleanup(curl);
 }
